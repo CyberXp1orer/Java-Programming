8a151560bde7b4d3bce4c99c9d05aadb64e456a8
Htet Kyaw Swar Linn <138316345+CyberXp1orer@users.noreply.github.com>
2023-07-02 00:30:36 +0700

    Delete ex3.java

--- ex3.java
+++
@@ -1,9 +0,0 @@
-import javax.sound.midi.Soundbank;
-
-public class ex3 {
-    public static void main(String[] args) {
-        double width = 4.5;
-        double height= 7.9;
-    System.out.println( width * height);
-    }
-}
