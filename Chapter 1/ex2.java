d43c247515911660913f1073620a80353a2b7a0b
Htet Kyaw Swar Linn <138316345+CyberXp1orer@users.noreply.github.com>
2023-07-02 00:31:47 +0700

    Add files via upload

---
+++ Chapter 1/ex2.java
@@ -0,0 +1,16 @@
+/*public class ex2 {
+    public static void main(String[] args) {
+        double answer = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+
+        System.out.print(" The answer is :"+answer);
+    }
+    
+}*/
+
+public class ex2 {
+    public static void main(String[] args) {
+
+        System.out.print((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5) );
+    }
+    
+}
---
+++ Chapter 1/ex3.java
@@ -0,0 +1,9 @@
+import javax.sound.midi.Soundbank;
+
+public class ex3 {
+    public static void main(String[] args) {
+        double width = 4.5;
+        double height= 7.9;
+    System.out.println( width * height);
+    }
+}
